d1aadc7208f0702d660a6bcf3eaaaef081b5df07
Ryan Carlos <ryancarlosnascimento243@gmail.com>
2025-04-07 21:45:59 -0300

    003 - feat: leitura de nome e sobrenome do usuário com exibição formatada

---
+++ 003-formatacao-de-nome-completo.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia o nome,
+# depois o sobrenome,
+# e retorne para o usuário
+
+#Leitura de dados
+nome = input('Digite seu nome: ')
+sobrenome = input('Digite seu sobrenome: ')
+
+#Retorno de dados
+print(f'Seu nome é: {nome} {sobrenome}')
