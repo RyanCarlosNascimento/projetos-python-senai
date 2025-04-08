766b1e0c17eb46d63fe31e823df8e63fb2e3a4b0
Ryan Carlos <ryancarlosnascimento243@gmail.com>
2025-04-07 21:42:02 -0300

    002 - feat: leitura de nome, idade e cidade de nascimento do usuário

---
+++ 002-leitura-nome-idade-cidade.py
@@ -0,0 +1,13 @@
+#Escreva um programa que leia,
+# o nome,
+# idade,
+# e cidade de nascimento
+# e retorne para o usuário
+
+#Leitura de dados
+nome = input('Digite seu nome: ')
+idade = int(input("Digite sua idade: "))
+cidade_de_nascimento = input("Digite a cidade em que você nasceu: ")
+
+#Retorno de informações
+print(f'Meu nome é: {nome}, tem atualmente {idade} anos e reside em {cidade_de_nascimento}')
