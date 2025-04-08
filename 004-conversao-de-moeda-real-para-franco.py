a4b5715ffecd61f8ed4559ccb00632e1891c3786
Ryan Carlos <ryancarlosnascimento243@gmail.com>
2025-04-07 21:48:33 -0300

    004 - feat: conversão de Real (BRL) para Franco Congolês (CDF)

---
+++ 004-conversao-de-moeda-real-para-franco.py
@@ -0,0 +1,8 @@
+# Escreva um programa que converta real para o Franco Congolês
+
+#Leitura de dados
+real = float(input('Digite o valor em reais: '))
+franco_congoles = real * 503.19
+
+#Retorno de dados
+print(f'O valor em Francos Congoleses após a conversão será de: Fr {franco_congoles:.2f}')
