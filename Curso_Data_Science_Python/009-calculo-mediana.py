52b2c06002cc97cf9b9d4532067d7dc987329b09
Ryan Carlos <ryancarlosnascimento243@gmail.com>
2025-05-15 23:57:21 -0300

    Adiciona exemplo de cálculo da mediana
    Código simples para calcular a mediana de uma lista numérica usando a biblioteca statistics.

---
+++ Curso_Data_Science_Python/009-calculo-mediana.py
@@ -0,0 +1,12 @@
+# Encontre a mediana:
+
+# Importação de bibliotecas
+import statistics
+
+#Lista com valores
+mediana = [7, 1, 3, 9, 5]
+
+# Calcular a mediana
+print(f'Mediana: {statistics.median(mediana):.2f}')
+
+
