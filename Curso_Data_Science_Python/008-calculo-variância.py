bd0f0dd6934a18cb44fba0ab65ecfec0ea3a6f1e
Ryan Carlos <ryancarlosnascimento243@gmail.com>
2025-05-15 23:56:28 -0300

    Adiciona cálculo de variância com statistics
    Exemplo de uso da função variance da biblioteca statistics para calcular a variância de um conjunto de dados.

---
+++ Curso_Data_Science_Python/008-calculo-variância.py
@@ -0,0 +1,11 @@
+# Calcule a variância
+
+#Importação da biblioteca
+import statistics
+
+#Lista com valores
+variancia = [5, 7, 8, 6, 9, 10]
+
+# Calcular a variância
+variancia = statistics.variance(variancia)
+print(f"Variância: {variancia:.2f}")
